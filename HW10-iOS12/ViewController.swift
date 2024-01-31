8d825b16945ea77e62bc4bc1c4970f7ef849c09b
mikwl <jauntymike@gmail.com>
2024-01-30 22:57:04 -0500

    Initial Commit

---
+++ HW10-iOS12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW10-iOS12
+//
+//  Created by Mikhail Parkhomchuk on 1/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
